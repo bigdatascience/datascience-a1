28ef55ed5305feccf0d590144e83047d3582e3cd
Adam Stirtan <adamstirtan@gmail.com>
2014-07-13 01:39:01 +0000

    back to default for part2

--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -1,28 +1,17 @@
 import sys
-import json
 
-
-def hw(sent_file, tweet_file):
-    scores = {};
-    for line in sent_file:
-        term, score = line.split("\t")
-        scores[term] = int(score)
-
-    for line in tweet_file:
-        message = json.loads(line)
-
+def hw():
+    print 'Hello, world!'
 
 def lines(fp):
     print str(len(fp.readlines()))
 
-
 def main():
     sent_file = open(sys.argv[1])
     tweet_file = open(sys.argv[2])
-    hw(sent_file, tweet_file)
+    hw()
     lines(sent_file)
     lines(tweet_file)
-
 
 if __name__ == '__main__':
     main()
