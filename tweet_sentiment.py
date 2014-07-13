ca2170100d316a9745684a60f7314fa67fb97faa
Adam Stirtan <adamstirtan@gmail.com>
2014-07-13 02:07:57 +0000

    part2 complete

--- tweet_sentiment.py
+++ tweet_sentiment.py
@@ -1,4 +1,5 @@
 import sys
+import json
 
 def hw():
     print 'Hello, world!'
@@ -12,6 +13,19 @@ def main():
     hw()
     lines(sent_file)
     lines(tweet_file)
+    scores = {}
+    for line in open(sys.argv[1]):
+        term, score = line.split("\t")
+        scores[term] = int(score)
+    for line in open(sys.argv[2]):
+        message = json.loads(line)
+        sentiment = 0
+        if "text" in message:
+            words = line.split(" ")
+            for word in words:
+                if word in scores:
+                    sentiment += scores[word]
+        print sentiment
 
 if __name__ == '__main__':
     main()
